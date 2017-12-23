be084f9aba2928aea83c6c2e7d8b7e491dded01d
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-23 09:31:37 +0100

    Plus onthoud welke operator je bent

--- Assets/bereken.cs
+++ Assets/bereken.cs
@@ -10,6 +10,29 @@ public class bereken : MonoBehaviour {
 	public InputField getal1;
 	public InputField getal2;
 
+	int welke = 0;
+
+	public void plus()
+	{
+
+		welke = 1;
+
+	}
+
+	public void min()
+	{
+		welke = 2;
+	}
+
+
+	public void keer()
+	{
+		welke = 3;
+
+	}
+
+
+
 
 	public void bereken_som()
 	{
@@ -22,10 +45,14 @@ public class bereken : MonoBehaviour {
 		int de_getal1_nummer = int.Parse (de_getal1_text);
 		int de_getal2_nummer = int.Parse (de_getal2_text);
 
-		int som = de_getal1_nummer + de_getal2_nummer;
+		int som_plus = de_getal1_nummer + de_getal2_nummer;
 
 
-		print (som);
+		if (welke == 1) {
+			print (som_plus);
+		} else {
+			print("Kies een operator");
+		}
 
 
 
