6b18d75551d8148e24d738824b8c280b31351f83
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-23 09:42:43 +0100

    - is mogelijk

--- Assets/bereken.cs
+++ Assets/bereken.cs
@@ -48,16 +48,22 @@ public class bereken : MonoBehaviour {
 		int de_getal2_nummer = int.Parse (de_getal2_text);
 
 		int som_plus = de_getal1_nummer + de_getal2_nummer;
+		int som_min = de_getal1_nummer - de_getal2_nummer;
 
 
 		String som_plus_text = som_plus.ToString ();
+		String som_min_text = som_min.ToString ();
 	
 
 		if (welke == 1) {
 			print (som_plus);
 			detext.text = som_plus_text;
+		} else if(welke == 2) {
+			print (som_min_text);
+			detext.text = som_min_text;
 		} else {
 			print("Kies een operator");
+			detext.text = "Kies een operator";
 		}
 
 
