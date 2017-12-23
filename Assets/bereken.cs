a78635995c4c8a48dd1f1d666fb13a0e072eef51
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-23 09:06:16 +0100

    Getal 1 en getal 2 zijn gevonden

--- Assets/bereken.cs
+++ Assets/bereken.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class bereken : MonoBehaviour {
+
+
+	public InputField getal1;
+	public InputField getal2;
+
+
 
 	// Use this for initialization
 	void Start () {
